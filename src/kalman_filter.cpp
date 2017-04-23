ce34a6ea1a66c21b6490ce58857f786019832fc6
Yi-Yuan Chiang <yychiang.vnu@gmail.com>
2017-04-23 23:00:28 +0800

    Add files via upload

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -22,6 +22,11 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  MatrixXd Ft = F_.transpose();
+
+  x_ = F_ * x_;
+  P_ = F_ * P_ * Ft + Q_;
+
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +34,26 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+
+  // z is the measured value of the sensors
+  H_ = MatrixXd(2, 4);
+  H_ << 1, 0, 0, 0,
+          0, 1, 0, 0;
+
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_ ;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+
+  long long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +61,28 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
+  // z is the measured value of the sensors
+  //Convert radar from polar to cartesian coordinates and initialize state.
+
+  double rho = sqrt((x_(0)*x_(0) + x_(1)*x_(1)));
+  double phi = atan2(x_(1),x_(0));
+  double rhorate= (x_(0)*x_(2) + x_(1)*x_[3])/sqrt((x_(0)*x_(0) + x_(1)*x_(1)));
+
+  // z_pred_radar: using above non-linear equations instead of H
+  MatrixXd  z_pred_radar(3, 1);
+  z_pred_radar << rho, phi, rhorate;
+
+  VectorXd y = z - z_pred_radar;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  size_t x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -5,6 +5,7 @@ using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
 
+using namespace std;
 Tools::Tools() {}
 
 Tools::~Tools() {}
@@ -15,6 +16,42 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+
+    // check the validity of the following inputs:
+    //  * the estimation vector size should not be zero
+    //  * the estimation vector size should equal ground truth vector size
+    if (estimations.size()==0){
+        cout << "The size of estimations.size()==0" <<endl;
+        return rmse;
+    }
+
+    if (estimations.size() != ground_truth.size()){
+        cout << "The estimation vector size should equal ground truth vector size"<<endl;
+        return rmse;
+    }
+
+    //accumulate squared residuals
+    //accumulate squared residuals
+    for(unsigned int i=0; i < estimations.size(); ++i){
+
+        VectorXd residual = estimations[i] - ground_truth[i];
+
+        //coefficient-wise multiplication
+        residual = residual.array()*residual.array();
+        rmse += residual;
+    }
+
+    //calculate the mean
+    rmse = rmse/estimations.size();
+
+    //calculate the squared root
+    rmse = rmse.array().sqrt();
+
+    //return the result
+    return rmse;
+
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +59,31 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+    MatrixXd Hj(3,4);
+    //recover state parameters
+    double px = x_state(0);
+    double py = x_state(1);
+    double vx = x_state(2);
+    double vy = x_state(3);
+
+    //pre-compute a set of terms to avoid repeated calculation
+    double c1;
+    c1 = px * px + py * py;
+    double c2;
+    c2 = sqrt(c1);
+    double c3;
+    c3 = (c1 * c2);
+
+    //check division by zero
+    if(fabs(c1) < 0.0001){
+        cout << "Division by Zero: in Tools::CalculateJacobian" << endl;
+        return Hj;
+    }
+
+    //compute the Jacobian matrix
+    Hj << (px/c2), (py/c2), 0, 0,
+            -(py/c1), (px/c1), 0, 0,
+            py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+
+    return Hj;
 }
